a4d5e79b0deaa23ded8bcecff5a774363cac5579
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-09-09 23:05:54 +0330

    Paginating coins data

--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -6,20 +6,22 @@ import Pagination from "../modules/Pagination";
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
-      setIsLoading(false)
+      setIsLoading(false);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <TableCoin coins={coins} isLoading={isLoading} />
-      <Pagination />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }
