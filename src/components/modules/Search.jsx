cd9acd973403af3a623888dc9882e553a567bd63
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-09-21 23:04:58 +0330

    Styling the searchBox

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { RotatingLines } from "react-loader-spinner";
 import { searchCoin } from "../../services/cryptoApi";
+import styles from "./Search.module.css";
 
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
@@ -40,7 +41,7 @@ function Search({ currency, setCurrency }) {
   }, [text]);
 
   return (
-    <div>
+    <div className={styles.searchBox}>
       <input
         type="text"
         placeholder="Search"
@@ -52,7 +53,7 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
-      <div>
+      <div className={styles.searchResult}>
         {isLoading && (
           <RotatingLines
             width="50px"
